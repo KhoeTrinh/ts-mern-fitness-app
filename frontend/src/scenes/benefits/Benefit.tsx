e4fe970872c42ac89692bb94b5bbd4dc46dfe5bf
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-13 13:20:05 +0700

    123

--- frontend/src/scenes/benefits/Benefit.tsx
+++ frontend/src/scenes/benefits/Benefit.tsx
@@ -1,13 +1,33 @@
-type Props = {};
+import { SelectedPage } from '@/shared/types';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-const Benefit = ({icon, title}: Props) => {
-  return <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
-    <div className="mb-4 flex justify-center">
-        <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
-            {icon}
+type Props = {
+    icon: JSX.Element;
+    title: string;
+    description: string;
+    SSP: (value: SelectedPage) => void;
+};
+
+const Benefit = ({ icon, title, description, SSP }: Props) => {
+    return (
+        <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
+            <div className='mb-4 flex justify-center'>
+                <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
+                    {icon}
+                </div>
+            </div>
+
+            <h4 className='font-bold'>{title}</h4>
+            <p className='my-3'>{description}</p>
+            <AnchorLink
+                className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+                onClick={() => SSP(SelectedPage.ContactUs)}
+                href={`${SelectedPage.ContactUs}`}
+            >
+                <p>Learn more</p>
+            </AnchorLink>
         </div>
-    </div>
-  </div>;
+    );
 };
 
 export default Benefit;
